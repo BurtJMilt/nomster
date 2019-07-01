976d4ee0d8e490c991a71cb538b5a4c9a7eccb5e
BurtJMilt <burtmiltenbeger.jm@gmail.com>
2019-07-01 00:49:39 +0000

    more photo failures

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,19 @@
 class PhotosController < ApplicatonController
+
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+	
 	def create
 		@place = Place.find(params[:place_id])
 		@place.photos.create(photos_params.merge(user: current_user))
 		redirect_to place_path(@place)
+		
+	end
+
+
+	private
+
+	def photo_params
 		params.require(:photo).permit(:caption, :picture)
+
 	end
 end
